cec6934563739b95333ed6389ab66fad3ddec64c
EdHer85 <113089146+EdHer85@users.noreply.github.com>
2025-06-25 21:03:01 -0600

    Primer commit

---
+++ frmPrincipal.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Test
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbSoloLetras = new System.Windows.Forms.TextBox();
+            this.txbSoloNumeros = new System.Windows.Forms.TextBox();
+            this.txbNoVacio = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Validación de controles:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Solo letras:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Solo números:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Texto no vacío:";
+            // 
+            // txbSoloLetras
+            // 
+            this.txbSoloLetras.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSoloLetras.Location = new System.Drawing.Point(166, 68);
+            this.txbSoloLetras.Name = "txbSoloLetras";
+            this.txbSoloLetras.Size = new System.Drawing.Size(420, 30);
+            this.txbSoloLetras.TabIndex = 4;
+            this.txbSoloLetras.TextChanged += new System.EventHandler(this.txbSoloLetras_TextChanged);
+            // 
+            // txbSoloNumeros
+            // 
+            this.txbSoloNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSoloNumeros.Location = new System.Drawing.Point(166, 104);
+            this.txbSoloNumeros.Name = "txbSoloNumeros";
+            this.txbSoloNumeros.Size = new System.Drawing.Size(420, 30);
+            this.txbSoloNumeros.TabIndex = 5;
+            this.txbSoloNumeros.TextChanged += new System.EventHandler(this.txbSoloNumeros_TextChanged);
+            // 
+            // txbNoVacio
+            // 
+            this.txbNoVacio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNoVacio.Location = new System.Drawing.Point(166, 140);
+            this.txbNoVacio.Name = "txbNoVacio";
+            this.txbNoVacio.Size = new System.Drawing.Size(420, 30);
+            this.txbNoVacio.TabIndex = 6;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 306);
+            this.Controls.Add(this.txbNoVacio);
+            this.Controls.Add(this.txbSoloNumeros);
+            this.Controls.Add(this.txbSoloLetras);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPrincipal";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbSoloLetras;
+        private System.Windows.Forms.TextBox txbSoloNumeros;
+        private System.Windows.Forms.TextBox txbNoVacio;
+    }
+}
+
---
+++ frmPrincipal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Test
+{
+    public partial class frmPrincipal: Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void txbSoloLetras_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txbSoloLetras.Text, "^[a-z]*$"))
+            {
+                MessageBox.Show("Por favor solo letras.");
+                txbSoloLetras.Text = string.Empty;
+            }
+
+        }
+
+        private void txbSoloNumeros_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txbSoloNumeros.Text, "^[0-9]*$"))
+            {
+                MessageBox.Show("Por favor solo números.");
+                txbSoloNumeros.Text = string.Empty;
+            }
+
+        }
+    }
+}
